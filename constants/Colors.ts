6facdf375c84dc6b6c55488dd63b721eaaff4be7
Mgachanja <markgachanja1@gmail.com>
2025-01-03 01:18:59 +0300

    milestone two complete

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -6,21 +6,20 @@
 const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
 
+
 export const Colors = {
-  light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
-  },
-};
+  primary: '#4CAF50', // Green
+  secondary: '#FF9800', // Orange
+  background: '#F5F5F5', // Light gray background
+  textPrimary: '#212121', // Dark gray text
+  textSecondary: '#757575', // Light gray text
+  border: '#E0E0E0', // Gray border
+  error: '#F44336', // Red for errors
+  success: '#4CAF50', // Green for success messages
+  warning: '#FFC107', // Yellow for warnings
+  info: '#2196F3', // Blue for informational messages
+  light: '#FFFFFF', // White
+  dark: '#000000', // Black
+} as const;
+
+export type ColorKeys = keyof typeof Colors;
